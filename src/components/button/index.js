fb368940fe0d01a848b95d7fd5f063f75c8fdad4
Conor McAfee <hello@conormcafee.co.uk>
2019-04-06 17:37:51 +0100

    Tidy up to What We Offer

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,50 +1,54 @@
-import React from "react";
-import styled from "styled-components";
-import Star from "../../images/icons/star.svg";
-import {FONT_BOLD} from "../../variables";
+import React from "react"
+import styled from "styled-components"
+import RightArrow from "../../images/icons/right-arrow.svg"
+import {FONT_BOLD} from "../../variables"
+
+const Button = (props) => (
+    <Btn onClick={props.onClick}>
+        <span>{props.children}</span>
+        <Icon src={RightArrow} alt="Arrow to indicate button is clickable" />
+    </Btn>
+)
+
+export default Button
 
 const Btn = styled.button`
     display: inline-flex;
     align-items: center;
     justify-content: center;
-    padding-top: 12px;
+    padding-top: 16px;
     padding-right: 24px;
-    padding-bottom: 12px;
+    padding-bottom: 16px;
     padding-left: 24px;
     border-radius: 25px;
-    background: rgb(156,233,207);
-    background: linear-gradient(90deg, rgba(156,233,207,1) 0%, rgba(111,216,186,1) 100%);
+    background-color: rgb(111,216,186);
+    background: linear-gradient(90deg, rgba(111,216,186,1) 0%, rgba(100,208,177,1) 100%);
     border: none;
     font-family: ${FONT_BOLD};
     font-weight: 900;
     color: #ffffff;
-    box-shadow: 0px 2px 1px 0px #66C9A8;
-    text-shadow: 1px 1px 1px rgba(0, 0, 0, .4);
     appearance: none;
-    transition: 
-        box-shadow 150ms ease-in-out,
-        text-shadow 150ms ease-in-out;
+    transition: background 150ms ease-in-out;
+
+    img {
+        transform: scale(0);
+        width: 0px;
+        transition: 
+            transform 150ms ease-in-out,
+            width 150ms ease-in-out;
+    }
 
     &:hover {
         cursor: pointer;
-        text-shadow: 1px 1px 1px #56ad90;
-        box-shadow: -1px 3px 0px 0px #56ad90;
+        background: linear-gradient(90deg, rgba(100,208,177,1) 0%, rgba(100,208,177,1) 100%);
+
+        img {
+            transform: scale(1);
+            width: 10px;
+        }
     }
-`;
+`
 
 const Icon = styled.img`
-    margin-right: 10px;
-`;
-
-class Button extends React.Component {
-    render() {
-        return (
-            <Btn>
-                <Icon src={Star} />
-                <span>{this.props.children}</span>
-            </Btn>
-        )
-    }
-} 
-
-export default Button;
+    margin-left: 10px;
+`
--- src/pages/what-we-offer/index.js
+++ src/pages/what-we-offer/index.js
@@ -1,14 +1,18 @@
 import React from "react"
-import {StaticQuery, graphql} from "gatsby"
+import {StaticQuery, graphql, navigate} from "gatsby"
+import styled from "styled-components"
 import {Flex, Box} from "@rebass/grid"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 import Container from "../../components/container"
-import Tagline from "../../components/tagline"
 import Button from "../../components/button"
 import test from "../../images/test-blob.svg"
+import {Heading02, Heading03} from "../../components/global/typography"
+
+const navigatePage = (page) => navigate(page)
 
 const Service = (props) => {
+    
     const { title, path, intro } = props.node.frontmatter
     const { index } = props 
     const width = [1,1/2]
@@ -18,32 +22,25 @@ const Service = (props) => {
 
     return (
         <Flex 
-            flexWrap={['wrap', 'nowrap']}
-            alignItems="center"
             as="section"
-            py={[5, 6]}
+            flexWrap={['wrap', 'nowrap']}
+            alignItems="center" py={[5]}
         >
             <Flex 
-                width={width}
-                px={padding}
-                flexDirection="column"
-                alignItems="center"
-                justifyContent="center"
+                width={width} px={padding} 
+                flexDirection="column" alignItems="center" justifyContent="center"
                 order={(index + 1) % 2 === 0 ? rtl[0] : ltr[0]}
             >
                 <img src={test} alt="Test"/>
             </Flex>
-            <Flex 
-                width={width} 
-                px={padding}
-                flexDirection="column"
-                justifyContent="center"
+            <Box 
+                width={width} px={padding}
                 order={(index + 1) % 2 === 0 ? rtl[1] : ltr[1]}
             >
-                <h1>{title}</h1>
+                <ServiceTitle>{title}</ServiceTitle>
                 <p>{intro}</p>
-                <Button>Find out More - {path}</Button>
-            </Flex>
+                <Button onClick={() => navigatePage(path)}>Find out More</Button>
+            </Box>
         </Flex>
     )
 }
@@ -61,14 +58,19 @@ const WhatWeOffer = (props) => {
                     <p>We believe that effective marketing begins with a clear strategy and a deep understanding of the customer.</p>
                     <p>From there we build an impressive brand identity, delivering creative and targeted messaging to the right customers at the right time.</p>
                 </Box>
+
+                <Flex flexWrap={['wrap']}>
+                    {services.map((service, index) => (
+                        <Box width={1/4} p={[3,4]} key={index}>
+                            <QuickLink onClick={() => navigate(service.node.frontmatter.path)} p={3}>
+                                <Heading03>{service.node.frontmatter.title}</Heading03>
+                            </QuickLink>
+                        </Box>  
+                    ))}
+                </Flex>
             </Container>
 
-            <Tagline />
-
-            <Flex 
-                as="section" 
-                flexDirection="column"
-            >
+            <Flex as="section" flexDirection="column" mx="auto" css={{ maxWidth: '1200px'}}>
                 {services.map((service, index) => (
                     <Service 
                         key={index} 
@@ -97,5 +99,24 @@ const services = graphql`
                 }
             }
         }
+    }
+`
+
+const ServiceTitle = styled(Heading02)`
+    margin-bottom: 0;
+`
+
+const QuickLink = styled.button`
+    border: 1px solid #e6e6e6;
+    text-align: center;
+    border-radius: 4px;
+    box-shadow: -2px 4px 2px 0 rgba(0, 0, 0, 0.05);
+    width: 100%;
+    background-color: #ffffff;
+    transition: background-color 150ms ease-in-out;
+
+    &:hover {
+        background-color: #f6f6f6;
+        box-shadow: 2px 4px -2px 0 rgba(0, 0, 0, 0.05);
     }
 `

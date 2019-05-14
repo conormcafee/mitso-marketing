992374674824f3315dacd3a021857c8018a5753c
Conor McAfee <hello@conormcafee.co.uk>
2019-05-14 20:07:10 +0100

    Additional Fields to Blog

--- src/templates/thoughts.js
+++ src/templates/thoughts.js
@@ -1,10 +1,18 @@
 import React from "react"
-import {Box} from "@rebass/grid"
+import { graphql } from "gatsby"
+import styled from "styled-components"
+import {Flex, Box} from "@rebass/grid"
 import SEO from "../components/seo"
 import Layout from "../components/layout"
 import Container from "../components/container"
 import WorkWithMitso from "../components/workWithMitso"
-import { graphql } from "gatsby"
+import { BLACK } from "../variables"
+import Tags from "../components/Tags"
+import Share from "../components/Share"
+import BackLink from "../components/BackLink"
+import Thoughts from "../components/Thoughts"
+import Image from "../images/backgrounds/hero-dummy.jpg";
+import BackgroundImage from "../components/BackgroundImage";
 
 const ThoughtsTemplate = (props) => {
   return (
@@ -14,19 +22,56 @@ const ThoughtsTemplate = (props) => {
 
 const Template = (props) => {
   const { markdownRemark } = props.data
-  const { frontmatter } = markdownRemark
+  const { frontmatter, html } = markdownRemark
   return (
-    <Layout>
-      <SEO title="Hi" />
+    <Layout dottedBackground>
+      <SEO title={`${frontmatter.title} | Thoughts by `} />
       <Container>
-          <Box px={[3,4]}>
-            <Box>
+        <Flex mb={5} px={[3,4]}>
+            <Hero>
+              <Title>{frontmatter.title}</Title>
+              <Tags tags={['Blog', 'Post', 'Test', 'Testing']} />
+            </Hero>
+          </Flex>
+
+          <Flex 
+            alignItems="center" 
+            justifyContent="space-between" 
+            mt={5} 
+            mb={3}
+            px={[3,4]}
+          >
+            <BackLink url="/thoughts" title="All Thoughts" />
+            <Share url="https://google.com" />
+          </Flex>
+
+          <BackgroundImage img={Image} aspectRatio />
+
+          <Caption 
+            as="div" 
+            alignItems="center" 
+            justifyContent="space-between"
+            px={[3,4]}
+          >
+            <h4>by Maeve Finnegan</h4>
+            <h4>1st May 2019</h4>
+          </Caption>
+
+          <Box px={[3,4]} mb={5}>
+            <Box as="article" mb={5}>
                 <h1>{frontmatter.title}</h1>
             </Box>
-          
-          </Box>          
+            
+            <Article dangerouslySetInnerHTML={{ __html: html }} />
+            
+            <Box mx="auto" mb={5} css={{ maxWidth: '700px' }}>
+              <Share url="https://google.com" />
+            </Box>
+
+            <Thoughts subPage/>
+          </Box>  
+        <WorkWithMitso />        
       </Container>
-      <WorkWithMitso />
     </Layout>
   )
 }
@@ -42,7 +87,48 @@ export const pageQuery = graphql`
       }
       frontmatter {
         title
+        date
       }
     }
   }
+`
+
+const Hero = styled(Box)`
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const Title = styled.h1`
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 32px;
+  margin-bottom: 16px;
+`
+
+const Article = styled(Box)`
+  line-height: 1.6; 
+
+  * {
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 700px;
+  }
+
+  p:first-of-type {
+    color: ${BLACK};
+    font-weight: bold;
+    font-size: 20px;
+  }
+
+  img {
+    display: block;
+    margin-top: 32px;
+    margin-bottom: 32px;
+  }
+`
+
+const Caption = styled(Flex)`
+  border-bottom: 2px solid #e6e6e6;
 `

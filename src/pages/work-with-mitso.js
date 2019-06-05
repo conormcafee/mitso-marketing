4018e2c792ed9e86d61f32a2cc491d6d5f43f871
Conor McAfee <hello@conormcafee.co.uk>
2019-06-05 18:36:11 +0100

    Removed type text for email

--- src/pages/work-with-mitso.js
+++ src/pages/work-with-mitso.js
@@ -43,7 +43,7 @@ export default ({ data }) => {
           </div>
           <div>
             <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" />
+            <input type="email" name="email" id="email" />
           </div>
           <div>
             <label htmlFor="message">Message</label>

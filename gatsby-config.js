f1b39b5ecdbe5489b0276c5c06fc6187a492fe2f
Conor McAfee <hello@conormcafee.co.uk>
2019-07-01 18:36:21 +0100

    Test Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,18 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-99218928-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "mitsomarketing.com",
+        trackingId: "UA-99218928-1"
       },
     },
   ],

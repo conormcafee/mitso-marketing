d7975ecefd7678efa2384d638b609fd7208953bc
Conor McAfee <hello@conormcafee.co.uk>
2019-08-28 21:46:15 +0100

    Added LinkedIn code

--- src/pages/success.js
+++ src/pages/success.js
@@ -19,6 +19,13 @@ export default () => (
       >
         <h1>Thanks, your message is on it's way</h1>
         <Button onClick={() => navigate("/")}>Back to Homepage</Button>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          alt=""
+          src="https://px.ads.linkedin.com/collect/?pid=1413180&conversionId=1288660&fmt=gif"
+        />
       </Wrapper>
     </Container>
   </Layout>
